a1d8a81159383d6de201303391d5ff0cdc39552d
1Hani-77 <haniasiri16@gmail.com>
2025-02-22 20:52:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def load_data():
     Q1 = df[['price', 'area']].quantile(0.05)
     Q3 = df[['price', 'area']].quantile(0.95)
     df = df[~((df[['price', 'area']] < (Q1 - 1.5 * (Q3 - Q1))) |
-             (df[['price', 'area']] > (Q3 + 1.5 * (Q3 - Q1))).any(axis=1)]
+             (df[['price', 'area']] > (Q3 + 1.5 * (Q3 - Q1))).any(axis=1))
     
     return df.dropna(subset=['price', 'area'])
 
